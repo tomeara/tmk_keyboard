d134035ac08dec90609d426db9336039954c4789
Ted O'Meara <ted@tedomeara.com>
2016-01-01 12:59:11 -0500

    used the ergodox config to make my own setup

--- keyboard/ergodox/keymap_tomeara.h
+++ keyboard/ergodox/keymap_tomeara.h
@@ -5,9 +5,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ESC,    1,    2,    3,    4,    5,  GRV,
     TAB,    Q,    W,    E,    R,    T, LBRC,
   LCTRL,    A,    S,    D,    F,    G,
-   LSFT,    Z,    X,    C,    V,    B, FN22,
+   LSFT,    Z,    X,    C,    V,    B, FN26,
     FN3,  FN2,  FN1, LALT, LGUI,
-                                      _MUTE,_VOLDOWN,
+                                        MUTE,VOLD,
                                              LALT,
                                   FN1, BSPC,  DEL,
 
@@ -15,18 +15,18 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          BSLS,    6,    7,    8,    9,    0, MINS,
          RBRC,    Y,    U,    I,    O,    P,  EQL,
                   H,    J,    K,    L, SCLN, QUOT,
-         FN23,    N,    M, COMM,  DOT, SLSH, RSFT,
+         FN27,    N,    M, COMM,  DOT, SLSH, RSFT,
                      RGUI, LEFT, DOWN,   UP, RGHT,
-  _VOLUP, STOP,
+   VOLU, MPLY,
    RALT,
     FN4,  ENT,  SPC),
 
   KEYMAP( // Layer 1
   // Left hand
    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
-    FN5, TRNS, FN19, TRNS, FN17, FN18, TRNS,
+    FN5, TRNS, FN19, TRNS, FN17, FN18, FN30,
    TRNS, FN14, FN15, FN16, FN10, TRNS,
-   TRNS,  FN6,  FN7,  FN8,  FN9, FN11, TRNS,
+   TRNS,  FN6,  FN7,  FN8,  FN9, FN11, FN28,
    TRNS, TRNS, TRNS, TRNS, TRNS,
                                        TRNS, TRNS,
                                              TRNS,
@@ -34,9 +34,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Right hand
          TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
-         TRNS, HOME, PGDN, PGUP,  END, FN13, TRNS,
+         FN31, HOME, PGDN, PGUP,  END, FN13, TRNS,
                LEFT, DOWN,   UP, RGHT, TRNS, TRNS,
-         TRNS, FN20, TRNS, TRNS, TRNS, TRNS, TRNS,
+         FN29, FN20, TRNS, TRNS, TRNS, TRNS, TRNS,
                      TRNS, TRNS, TRNS, TRNS, TRNS,
    TRNS, TRNS,
    TRNS,
@@ -51,7 +51,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    TRNS, TRNS, TRNS, TRNS, TRNS,
                                        ACL0, ACL1,
                                              TRNS,
-                                 TRNS, TRNS, TRNS,
+                                 BTN1, BTN2, TRNS,
 
   // Right hand
          TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
@@ -61,7 +61,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      TRNS, TRNS, TRNS, TRNS, TRNS,
    ACL2, TRNS,
    TRNS,
-   TRNS, BTN1, BTN2),
+   TRNS, TRNS, TRNS),
 
   KEYMAP( // Layer 3
   // Left hand
@@ -95,7 +95,7 @@ static const uint16_t PROGMEM fn_actions[] = {
   ACTION_LAYER_MOMENTARY(1),
   ACTION_LAYER_MOMENTARY(2),
   ACTION_LAYER_MOMENTARY(3),
-  ACTION_LAYER_TOGGLE(2),
+  ACTION_LAYER_MOMENTARY(2),
   ACTION_MODS_KEY(MOD_LGUI, KC_TAB),
   ACTION_MODS_KEY(MOD_LGUI, KC_Z),
   ACTION_MODS_KEY(MOD_LGUI, KC_X),
@@ -113,8 +113,16 @@ static const uint16_t PROGMEM fn_actions[] = {
   ACTION_MODS_KEY(MOD_LGUI, KC_W),
   ACTION_MODS_KEY(MOD_LGUI, KC_N),
   ACTION_MODS_KEY(MOD_LGUI, KC_SPC),
-  ACTION_MODS_KEY(MOD_LSFT, KC_9),
+  ACTION_MODS_KEY(MOD_LCTL, KC_J),
+  ACTION_MODS_KEY(MOD_LCTL, KC_K),
+  ACTION_MODS_KEY(MOD_LCTL, KC_L),
+  ACTION_MODS_KEY(MOD_LCTL, KC_SCLN),
+  ACTION_MODS_KEY(MOD_RSFT, KC_9),
   ACTION_MODS_KEY(MOD_LSFT, KC_0),
+  ACTION_MODS_KEY(MOD_RSFT, KC_COMM),
+  ACTION_MODS_KEY(MOD_LSFT, KC_DOT),
+  ACTION_MODS_KEY(MOD_RSFT, KC_LBRC),
+  ACTION_MODS_KEY(MOD_LSFT, KC_RBRC)
 };
 
 void action_function(keyrecord_t *event, uint8_t id, uint8_t opt)
